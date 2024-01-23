03e912298c6c661a141c8eac949f9e906992deaf
ArtusAbelard <tribelha@gmail.com>
2024-01-23 16:39:27 +0100

    add comentary

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,18 @@
 let btnajouter = document.querySelector("#btnajouter")
 let inputtodo = document.querySelector("#inputtodo")
-let body = document.querySelector("body")
+let body = document.querySelector("body")                //ciblage des ellements
 let filtres = document.querySelectorAll(".btnhover")
 
 
-
-
-
-
 function addtodo() {
-    
+
     let tododiv = document.createElement("div")
     tododiv.classList.add("tododiv")
     
     let titretask = document.createElement("h2")
     titretask.classList.add("titletask")
     titretask.innerText = inputtodo.value
-    tododiv.appendChild(titretask)
+    tododiv.appendChild(titretask)                    //fonction pour cree les div , h2 , button
 
     let btndiv = document.createElement("button")
     btndiv.classList.add("btnvalider")
@@ -36,76 +32,56 @@ function addtodo() {
     let valider
 
     btndiv.addEventListener('click', ()=>{
-        // tododiv.style.background= "#00ff04"
         tododiv.classList.toggle("letoggle")
         console.log(tododiv.classList);
-        valider = "valider"
+        valider = "valider"                    //btn valider
         tododiv.style.display="block"
-        // btndiv.addEventListener("click",()=>{
-        //     tododiv.style.display=""
-            // btndiv.addEventListener('click', ()=>{
-            //     tododiv.style.background= "#00ff04"})
-            //     tododiv.style.display="block"
-        // })
      
     })
     
     btndiv1.addEventListener('click', ()=>{
-        titretask.innerText = inputtodo.value
+        titretask.innerText = inputtodo.value         //btn modifier
         inputtodo.value=""
-    })
-    btndiv2.addEventListener('click',()=>{
+    })                                                   
+    btndiv2.addEventListener('click',()=>{             //btn supprimer
         tododiv.remove()
     })
 
     filtres[0].addEventListener('click', ()=>{
-        if (tododiv.classList=="tododiv letoggle") {
-            tododiv.style.display = "block"
+        if (tododiv.classList=="tododiv letoggle") {         
+            tododiv.style.display = "block"               //btn filtre tout 
         }else{
             tododiv.style.display = "block"
-
         }
-
     })
 
     filtres[1].addEventListener('click', ()=>{
-        if (tododiv.classList=="tododiv letoggle") {
+        if (tododiv.classList=="tododiv letoggle") {        //btn filtre en cours
             tododiv.style.display = "none"
         }else{
             tododiv.style.display = "block"
-
         }
 
     })
-    "tododiv letoggle"
+   
 
     filtres[2].addEventListener("click", ()=>{
-        if (tododiv.classList=="tododiv letoggle") {
+        if (tododiv.classList=="tododiv letoggle") {       //btn filtre valider
             tododiv.style.display = "block"
         }else{
             tododiv.style.display = "none"
         }
-
     })
-
-    // filtres[2].addEventListener("click", ()=>{
-    //     if (valider=="valider") {
-    //         tododiv.style.display = "block"
-    //     }else{
-    //         tododiv.style.display = "none"
-    //     }
-
-    // })
-    body.appendChild(tododiv)
-    inputtodo.value=""
+    body.appendChild(tododiv)  //creation de la div dans le body
+    inputtodo.value=""        //vider linput
 }
 
-btnajouter.addEventListener('click',addtodo)
+btnajouter.addEventListener('click',addtodo)  // addevent qui appelle la fonction pour cree les div, ...
 
 
 inputtodo.addEventListener("keydown", (ev) => {
     console.log(ev.key);
-    if (ev.key == 'Enter' ){
+    if (ev.key == 'Enter' ){   // utiliser la touche entree pour trigger l input
         addtodo()
     }
 })
